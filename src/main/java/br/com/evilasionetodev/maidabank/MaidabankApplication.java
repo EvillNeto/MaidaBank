dd2a8b590b879932931759f1ebfd08b6c6bff6a1
Evilásio Neto <evilasio_neto12@hotmail.com>
2021-08-23 19:45:43 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/evilasionetodev/maidabank/MaidabankApplication.java
@@ -0,0 +1,13 @@
+package br.com.evilasionetodev.maidabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaidabankApplication.class, args);
+	}
+
+}
